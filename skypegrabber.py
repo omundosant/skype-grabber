f8c38b92d0a9fbb92723795431a2856a9cadb379
Sant <35517553+omundosant@users.noreply.github.com>
2018-02-21 19:08:35 -0300

    Create skypegrabber.py

---
+++ skypegrabber.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Sant @omundosant
+__________________
+
+Copia não comédia
+"""
+
+import skype4py
+import socket
+import struct
+import os
+import sys
+import time
+import datetime
+
+from socket import SOCK_DGRAM, AF_INET
+
+today = datetime.date.today()
+
+MY_IP = socket.gethostname(socket.gethostbyname())
+STARTED = bool()
+SIZE_DIFF = f.size()
+ALL_CONNECTIONS = socket.interface(netifaces.interfaces())
+FILTER_IPADD = IPAddress[0]
+FILTER_IP = bool()
+
+from MY_IP import IPAddress
+from STARTED import Boolean
+from SIZE_DIFF import Size
+from ALL_CONNECTIONS import *
+from FILTER_IPADD import *
+from FILTER_IP import Boolean
+
+IP_FROM = IPAddress()
+IP_TO = IPAddress()
+BYTE_DATA = [4096]
+
+DES_PORT = uniform(str)
+SRC_PORT = uniform(str)
+
+lbCapturado = ("[>>] IP Capturado ", ":%s".format(today))
+lbError = ("[!!] Não foi possivel capturar IP, tente reiniciar o APP!", sys.exit(1))
+
+ctrIPResolver = ("[#] Lista de IP Capturados ->\n[{}]").format(sys.argv[1])
+
+ def ListIP(Control = ctrIPResolver):
+          if STARTED:
+          	READ_LENGHT = uniform(BitConverter.ToSingle(16, bytez), 0)
+          	SRC_PORT = BitConverter.ToSingle(16, bytez(BYTE_DATA, 22), 0)
+            DES_PORT = BitConverter.ToSingle(16, bytez(BYTE_DATA, 24), 0)
+
+            IP_FROM = IPAddress(BitConverter.ToSingle(32, bytez, 12))
+            IP_TO = IPAddress(BitConverter.ToSingle(32, bytez, 16))
+
+            ListIP.ctrIPResolver("\n:\n"(sizeof))
+          break;
+          socket.socket(NetworkToHostOrder(BYTE_DATA, 0, BYTE_DATA.lenght, flags="FA", AsyncCallBack(sizeof), not))
+
+if __ListIP__ == '__ListIP__':
+           ListIP()                      
+
+def RESULT_UPDATE():
+       IPV4 = raw_input("[::] Digite seu Ipv4 -> ")
+       PORTA = raw_input("[::] Porta do Skype -> ")
+       if BYTE_DATA[9]:
+       	IP_FROM.format(str, IPV4 + DES_PORT=80 + IP_TO.format_startswith("104") = False)
+         if IP_TO.format_startswith("40.")
+              ListIP += str.format("{0}{1}", IP_TO.format, str)
+              print("[!] Extraimos um IP do Skype.")
+              os.system("cls")
+              input("[>>] Aperte alguma tecla para continuar")
+              STARTED = False
+         break; 
+             ListIP += (str.format("{0}{1}", IP_TO.format, ":" + DES_PORT))
+             print(lbCapturado + ":" + IP_TO.format + ":" + DES_PORT)
+             sys.exit(1)
+             STARTED = False
+         break;
+         
+def Byteswap(bytez, uniform):	
+   from result[0] import Byte  
+   result[0] = bytez(+1)
+   result[1] = bytez[0]
+   return result;
+
+def bindsocket():
+   socket.bind(IPAddress(MY_IP, PORTA))
+   socket.setsockopt(socket.D_GRAM, cannoname.header(True))
+   from bytrue[] import Byte *
+   from byout[] import Byte * 
+   Byte = [{1, 0, 0, 0,}, {1, 0, 0, 0,}]    
+   socket.ioctl(socket.ioctl_recv, bytrue, byout)
+   socket.setblocking(False)
+   if BYTE_DATA(socket.recv_into):
+   	  socket.socket(NetworkToHostOrder(BYTE_DATA, 0, BYTE_DATA.lenght, flags="None", AsyncCallBack(), not))
+   else:
+      print("[!!] Skype Grabber sera reiniciado!", sys.argv[3], time.sleep(3), sys.exit(1))
+
+if __bindsocket__ == '__bindsocket__':
+                   	bindsocket()                   
